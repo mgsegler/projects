eee3dccd8dd7f549c1ca1d3af0665545b8c38957
mgsegler <mgsegler@gmail.com>
2019-03-11 09:03:18 -0500

    new file:   bebop_ws/src/bebop_autonomy 	new file:   bebop_ws/src/bebop_control/CMakeLists.txt 	new file:   bebop_ws/src/bebop_control/package.xml 	new file:   bebop_ws/src/bebop_control/src/bebop_control test.py 	new file:   bebop_ws/src/bebop_control/src/bebop_control.py 	modified:   bebop_ws/src/bebop_autonomy (modified content, untracked content)

---
+++ bebop_ws/build/BebopControl/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/leader/bebop_ws/src/BebopControl
+# Build directory: /home/leader/bebop_ws/build/BebopControl
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ bebop_ws/build/bebop_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/leader/bebop_ws/src/bebop_control
+# Build directory: /home/leader/bebop_ws/build/bebop_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
